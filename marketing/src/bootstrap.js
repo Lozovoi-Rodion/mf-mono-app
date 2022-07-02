60c78a59f13b1edad2c52b2414ab69841ff67a7f
Lozovoi-Rodion <rodionlozovoi@gmail.com>
2022-07-02 21:05:29 +0200

    added marketing components

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
+// Mount function to start up the app
 const mount = (el) => {
-  ReactDOM.render(<h1>Hi there</h1>, el);
+  ReactDOM.render(<App />, el);
 };
 
-if (process.env.NODE === "development") {
+// If we are in development and in isolation,
+// call mount immediately
+if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
 
   if (devRoot) {
@@ -13,4 +17,6 @@ if (process.env.NODE === "development") {
   }
 }
 
+// We are running through container
+// and we should export the mount function
 export { mount };

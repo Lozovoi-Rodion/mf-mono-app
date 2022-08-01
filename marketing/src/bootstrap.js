b6dd034d4cc9f39b9c59d5218085e540e6f9670f
Lozovoi-Rodion <rodionlozovoi@gmail.com>
2022-08-01 08:33:47 +0200

    work on inner&outer routing

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createMemoryHistory } from "history";
+import App from "./App";
 
 // Mount function to start up the app
 const mount = (el) => {
-  ReactDOM.render(<App />, el);
+  // check it. probaly nesting is enough https://v5.reactrouter.com/web/example/nesting
+  const history = createMemoryHistory();
+  ReactDOM.render(<App history={history} />, el);
 };
 
 // If we are in development and in isolation,
 // call mount immediately
-if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root');
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_marketing-dev-root");
 
   if (devRoot) {
     mount(devRoot);

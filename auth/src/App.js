a71a7fd7a3d1a175f953bb5c6b26d831bd979ed7
Lozovoi-Rodion <rodionlozovoi@gmail.com>
2022-08-01 10:58:26 +0200

    adding auth app

---
+++ auth/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Switch, Route, Router } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "au",
+});
+
+export default ({ history }) => {
+  console.log(history)
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <Router history={history}>
+          <Switch>
+          <Router path="/auth/signin" component={Signin} />
+          <Router path="/auth/signup" component={Signup} />
+          </Switch>
+        </Router>
+      </StylesProvider>
+    </div>
+  );
+};
